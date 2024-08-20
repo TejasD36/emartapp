5d26a3a046a3db0a1aefc77d274bc84684cad4d8
Tejas Dudhal <116336036+TejasD36@users.noreply.github.com>
2024-08-20 11:32:54 +0530

    Sub Category Screen UI done

--- lib/views/category/category_details.dart
+++ lib/views/category/category_details.dart
@@ -26,7 +26,6 @@ class _CategoryDetailsState extends State<CategoryDetails> {
           padding: EdgeInsets.all(12),
           child: Column(
             children: [
-
               //SubCategories
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
@@ -52,7 +51,52 @@ class _CategoryDetailsState extends State<CategoryDetails> {
               20.heightBox,
 
               //All Products Section
-
+              Expanded(
+                child: GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      mainAxisExtent: 250,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            imgP5,
+                            height: 150,
+                            width: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          10.heightBox,
+                          "Laptop"
+                              .text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .make(),
+                          10.heightBox,
+                          "Price"
+                              .text
+                              .fontFamily(bold)
+                              .color(redColor)
+                              .size(16)
+                              .make(),
+                          10.heightBox,
+                        ],
+                      )
+                          .box
+                          .white
+                          .roundedSM
+                          .padding(const EdgeInsets.all(12))
+                          .outerShadowSm
+                          .make();
+                    }),
+              ),
             ],
           ),
         ),
